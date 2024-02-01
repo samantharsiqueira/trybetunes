b473bb7a0cb27b7a93304025b6ba217904973a19
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-02-01 19:48:42 +0000

    tentando a primeira

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
+import { Login, Search, Album, Favorites, Profile, ProfileEdit, NotFound } from '../pages';
 
 function App() {
   return (
+    <>
     <p>Trybetunes</p>
-    
+    <Routes>
+      <Route path='/' element= { Login />} />
+      <Route path='/search' element= { Search />} />
+      <Route path='/album/:id' element= { Album />} />
+      <Route path='/favorites' element= { Favorites />} />
+      <Route path='/profile' element= { Profile />} />
+      <Route path='/profile/edit' element= { ProfileEdit />} />
+      <Route path='*' element= { NotFound />} />
+    </Routes>
+    </>
   );
 }
 
