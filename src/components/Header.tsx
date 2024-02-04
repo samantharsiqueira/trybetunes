bde74f20932a15b91a8a8e695db04b3067008111
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-02-04 15:45:10 +0000

    atualizando os comentários

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,23 @@ import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
 function Header() {
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState(''); // Armazena o nome do usuário
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    // Define uma função assincrona
     async function fetchUser() {
-      const user = await getUser();
-      setUserName(user.name);
-      setLoading(false);
+      // Chama a função que retorna uma promisse
+      const user = await getUser(); // Espera até ter as informaçoes do user e coloca nessa variavel pra gente usar no código
+      setUserName(user.name); // Atualiza o estado com o nome do usuário
+      setLoading(false); // Para de aparecer o carregando
     }
+    // Chama a função assincrona
     fetchUser();
-  }, []);
+    // O segundo argumento é uma array vazio, indica que o efeito deve ser executado apenas uma vez ao montar o componente
+  }, []); // Só vai pegar o usuário quando montar pela primeira vez.
 
+  // Retorna a estrutura do componente
   return (
     <header
       data-testid="header-component"
